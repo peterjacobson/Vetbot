6e03498e84da3f33cd56ed6f1833b674de92277d
mohamedMok <Mohamed@MacBook-Air-de-Mohamed.local>
2015-12-08 16:38:18 +1300

    VetBot 1.2.0

--- js/controllers/details.js
+++ js/controllers/details.js
@@ -4,6 +4,7 @@ app.controller('Submission', ['$scope','$location', '$http', function ($scope, $
 	
 	var idsubmissions = JSON.parse(sessionStorage.getItem('idConsentSelected'));
 	$scope.submissions = {};
+	$scope.deniedDescription = "";
 
 	$scope.init = function(){
 		var res = $http.get("http://ec2-52-18-99-146.eu-west-1.compute.amazonaws.com/submissions/" + idsubmissions,{ headers: {'Authorization': 'Basic dGF1cG8tY291bmNpbEB0ZGMuY28ubno6dGF1cG9jb3VuY2ls' } });
@@ -26,21 +27,38 @@ app.controller('Submission', ['$scope','$location', '$http', function ($scope, $
       'from_email': 'mohamed@vizbot.co.nz',
       'to': [
           {
-            'email': 'matt@vizbot.co.nz',
-            'type': 'to'
-          },
-          {
-            'email': 'samson@vizbot.co.nz',
+            'email': 'buildingconsents@taupo.govt.nz',
             'type': 'to'
           }
         ],
       'autotext': 'true',
-      'subject': 'First mail from VetBot',
-      'html': '<h1>Hey Guys</h1><br/>This is the first mail from vizbot :)'
+      'subject': 'Vetting Process',
+      'html':'<h3>Dear building consents team</h3><br/>We would like to inform you that: <br/>' + $scope.submissions.title + '<br/>'+ $scope.submissions.address + '<br/>'+'has been vetted by someone in your team via VetBot.<br/>'+'The agent has been notified.<br/>'+'<h3>Next steps: </h3>'+'Please check in your internal system to process this application as per usual.'+'<br/>'+'<br/>'+'Cheers,<br/>'+'Vizbot'
     }
   }
  }).done(function(response) {
+   $('#acceptedBox').modal('hide');
+   var res = $http.post("http://ec2-52-18-99-146.eu-west-1.compute.amazonaws.com/submissions/" + idsubmissions + "/accepted", "" ,{ headers: {'Authorization': 'Basic dGF1cG8tY291bmNpbEB0ZGMuY28ubno6dGF1cG9jb3VuY2ls' } });
+		res.success(function(data, status, headers, config) {
+			console.log("Vetting done");
+		});
+		res.error(function(data, status, headers, config) {
+			// Handle error 
+		});	
    console.log(response); // if you're into that sorta thing
  });
 	};
+
+	$scope.deny = function(){
+		var res = $http.post("http://ec2-52-18-99-146.eu-west-1.compute.amazonaws.com/submissions/" + idsubmissions + "/denied",$scope.deniedDescription, { headers: {'Authorization': 'Basic dGF1cG8tY291bmNpbEB0ZGMuY28ubno6dGF1cG9jb3VuY2ls' } });
+		res.success(function(data, status, headers, config) {
+			console.log("done");
+		});
+		res.error(function(data, status, headers, config) {
+			// Handle error 
+		});
+
+	};
+
 }]);
+
--- js/controllers/home.js
+++ js/controllers/home.js
@@ -1,7 +1,9 @@
 "use strict";
 
-app.controller('HomeCtrl', ['$scope','$location', '$http', function ($scope, $location, $http) {
+app.controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
+
 	$scope.submissions = [];
+	
 	$scope.init = function(){
 		var res = $http.get("http://ec2-52-18-99-146.eu-west-1.compute.amazonaws.com/submissions",{ headers: {'Authorization': 'Basic dGF1cG8tY291bmNpbEB0ZGMuY28ubno6dGF1cG9jb3VuY2ls' } });
 		res.success(function(data, status, headers, config) {
